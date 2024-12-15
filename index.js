9684f40bb55792e60741315d3bea275e77b6d9e1
Asbjørn Enge <asbjorn@hanafjedle.net>
2024-12-15 11:52:50 +0100

    Keep domain in redirect location if resolved

--- index.js
+++ index.js
@@ -19,11 +19,16 @@ dns.setServers(['8.8.8.8'])
 app.use(async (req, res, next) => {
   console.log(req.path)
   let path = req.path
+  let rhash = ''
+  let domain = ''
+  let resolved = false
   if (req.path.indexOf('/@') === 0) {
     try {
-      const domain = req.path.slice(2).split('/')[0]
+      domain = req.path.slice(2).split('/')[0]
       const dhash = await dns.resolve(`_onchfs.${domain}`, 'TXT')
-      path = path.replace(`@${domain}`, dhash[0][0].split('onchfs://')[1])
+      rhash = dhash[0][0].split('onchfs://')[1]
+      path = path.replace(`@${domain}`, rhash)
+      resolved = true
     } catch(e) {
       console.log(e)
       return res.status(404).send('DNS resolution failed')
@@ -31,6 +36,10 @@ app.use(async (req, res, next) => {
   }  
   console.log(path)
   const response = await resolver(path)
+  if (response.status === 308 && resolved) {
+    response.headers['Location'] = response.headers['Location'].replace(rhash, `@${domain}`)
+  }
+  console.log(response.headers, response.status)
   return res
     .header(response.headers)
     .status(response.status)
